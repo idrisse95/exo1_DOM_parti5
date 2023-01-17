4b00d4a18dca7673f9d648cd19c1e2f28ea3a4bf
55973 <55973@etu.he2b.be>
2023-01-17 16:24:08 +0100

    fin

---
+++ main.js
@@ -0,0 +1,41 @@
+// > Event Target
+
+// ### 1. Créer un programme qui te permet de passer le contenu de la variable pContent jusqu'a une function et en suite fait en sorte que cette function ajoute ce qu'elle a reçue en parametre dans le p de la section, sert toi du button Exo1
+
+
+// ### 2. Créer un programme qui au clique du h1, le background devient gold et sa color red, Attention voici les contraintes :
+// - Les couleurs sont contenu dans des variables sur le fichier main.js, sert-en pour les affecter au style de la balise h1
+// - Il faut que ces variables soient envoyés en parametre dans la function via le addEventListener.
+
+// Pour l'exercice 2
+let theColor = "red";
+let theBackground = "gold";
+let pContent = "Il est possible de passer des parametres dans la function utilisé dans l'écouteur d'événement !"
+
+
+let exo1 = document.querySelector('input') 
+
+exo1.addEventListener('click', ()=> {
+
+    // ### 1. Créer un programme qui te permet de passer le contenu de la variable pContent jusqu'a une function et en suite fait en sorte que cette function ajoute ce qu'elle a reçue en parametre dans le p de la section, sert toi du button Exo1
+
+    
+
+    let p = document.querySelector('p') 
+
+    p.innerHTML = pContent    
+   
+   console.log(p);
+
+});
+
+
+
+
+// ### 2. Créer un programme qui au clique du h1, le background devient gold et sa color red, Attention voici les contraintes :
+// - Les couleurs sont contenu dans des variables sur le fichier main.js, sert-en pour les affecter au style de la balise h1
+// - Il faut que ces variables soient envoyés en parametre dans la function via le addEventListener.
+let exo2 = document.querySelector('h1')
+exo2.addEventListener('click', () =>{
+    exo2.style.backgroundColor=theBackground
+})
